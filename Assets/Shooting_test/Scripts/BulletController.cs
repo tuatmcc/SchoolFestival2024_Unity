e8161b564148ffe5db3799ccb8cfc6aa2fa60c48
Inoyu <117442042+Xia7an@users.noreply.github.com>
2024-09-13 12:04:22 +0900

    shooting
    だいたいできたかも

---
+++ Assets/Shooting_test/Scripts/BulletController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    private Vector3 velocity;
+    private Rigidbody rb;
+    private Vector3 normal;
+    private int reflect_count = 0;
+    private int max_reflect_num = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this.GetComponent<Rigidbody>().AddForce(new Vector3(0,0,4),ForceMode.Impulse);
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        velocity = rb.velocity;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("壁衝突");
+        if (collision.gameObject.CompareTag("Border"))
+        {
+            if (velocity.magnitude <= 0.1)
+            {
+                Destroy(this.gameObject);
+            }
+            reflect_count++;
+            normal = collision.contacts[0].normal;
+
+            Vector3 result = Vector3.Reflect(velocity, normal);
+
+            rb.velocity = result;
+
+            // directionの更新
+            velocity = rb.velocity;
+        }
+        if (reflect_count == max_reflect_num+1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Shooting_test/Scripts/BulletRayManager.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Shooting_test
+{
+    public class BulletRayManager : MonoBehaviour
+    {
+        [SerializeField]
+        public Transform shootPoint;
+
+        PlayerInput playerInput;
+        LineRenderer linerend;
+        private float BULLET_RADIUS = 0.15f;
+        private void Start()
+        {
+            playerInput = this.GetComponent<PlayerInput>();
+            linerend = this.GetComponent<LineRenderer>();
+            linerend.positionCount = 0;
+        }
+
+        private void Update()
+        {
+            if (playerInput.actions["Draw_Ray"].IsPressed())
+            {
+                Draw_Bullet_shot(shootPoint);
+                Debug.Log("押している");
+            }
+        }
+
+        public void Draw_Bullet_shot(Transform ShootPoint)
+        {
+            var direction = shootPoint.rotation * Vector3.forward;
+            Ray ray1 = new Ray(ShootPoint.position, direction),
+                ray2 = new Ray(ShootPoint.position, direction),
+                ray3 = new Ray(ShootPoint.position, direction),
+                ray4 = new Ray(ShootPoint.position, direction);
+            Ray[] rays = new Ray[4];
+            RaycastHit[] hits = new RaycastHit[5];
+            rays[0] = new Ray(ShootPoint.position, direction);
+            if (Physics.SphereCast(rays[0], BULLET_RADIUS, out hits[0]))
+            {
+                rays[1] = new Ray(hits[0].point, Vector3.Reflect(direction, hits[0].normal));
+            }
+            if (Physics.SphereCast(rays[1], BULLET_RADIUS, out hits[1]))
+            {
+                rays[2] = new Ray(hits[1].point, Vector3.Reflect(hits[1].point - hits[0].point, hits[1].normal));
+            }
+            if (Physics.SphereCast(rays[2], BULLET_RADIUS, out hits[2]))
+            {
+                rays[3] = new Ray(hits[2].point, Vector3.Reflect(hits[2].point - hits[1].point, hits[2].normal));
+            }
+            Physics.SphereCast(rays[3], BULLET_RADIUS, out hits[3]);
+            DrawRayLine(ShootPoint.position, hits);
+        }
+
+        private void DrawRayLine(Vector3 start, RaycastHit[] hits)
+        {
+            //LineRendererコンポーネントの取得
+            linerend.startWidth = 3;
+            linerend.endWidth = 3;
+            linerend.positionCount = 5;
+
+            //線の太さを設定
+            linerend.startWidth = 0.1f;
+            linerend.endWidth = 0.1f;
+
+            //始点, 終点を設定し, 描画
+            linerend.SetPosition(0, start);
+            linerend.SetPosition(1, hits[0].point);
+            linerend.SetPosition(2, hits[1].point);
+            linerend.SetPosition(3, hits[2].point);
+            linerend.SetPosition(4, hits[3].point);
+        }
+
+        public void Delete_BulletShot()
+        {
+            linerend.positionCount = 0;
+        }
+    }
+}
