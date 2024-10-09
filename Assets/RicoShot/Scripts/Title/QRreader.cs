9616a2312ac583db188b9d62b286e7f4175e2cc8
toufu24 <120612884+toufu-24@users.noreply.github.com>
2024-10-09 17:15:34 +0900

    ボタンが押せない問題を修正

--- Assets/RicoShot/Scripts/Title/QRreader.cs
+++ Assets/RicoShot/Scripts/Title/QRreader.cs
@@ -1,15 +1,14 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Serialization;
 using ZXing;
 
 namespace RicoShot.Title
 {
     public class QRreader : MonoBehaviour
     {
-        // カメラ許可のための定数定義
-        private const string Permission = UnityEngine.Android.Permission.Camera;
-
         [SerializeField] private TextMeshProUGUI resultText; // QRコードの読み取り結果を表示するText
         [SerializeField] private RawImage webCameraRawImage; // Webカメラの映像を表示するRawImage
 
@@ -20,42 +19,37 @@ namespace RicoShot.Title
         private RawImage _rawImage;
         private WebCamTexture _webCamTexture = null;
 
-        // アプリ起動時に実行される初期化処理
-        private void Awake()
+        [SerializeField] private Button changeCameraButton;
+
+        private void Start()
         {
-            // カメラの使用許可をユーザーにリクエスト
-            UnityEngine.Android.Permission.RequestUserPermission(Permission);
+            // カメラ切り替えボタンにChangeCameraメソッドを登録
+            changeCameraButton.onClick.AddListener(ChangeCamera);
+            
+            // 利用可能なカメラデバイスを取得
+            _webCamDevice = WebCamTexture.devices;
+
+            // Webカメラの映像を取得するために新しいWebCamTextureを作成
+            _webCamTexture = new WebCamTexture(_webCamDevice[0].name);
+
+            // Webカメラの映像をスタート
+            _webCamTexture.Play();
+
+            // Webカメラの映像をRawImageに表示
+            webCameraRawImage.texture = this._webCamTexture;
         }
 
         private void Update()
         {
-            // Webカメラがまだセットされていない場合
-            if (_webCamTexture == null)
-            {
-                // カメラの使用がユーザーによって許可されているかチェック
-                if (UnityEngine.Android.Permission.HasUserAuthorizedPermission(Permission))
-                {
-                    // デバイスのスクリーンの幅と高さを取得
-                    var width = Screen.width;
-                    var height = Screen.height;
-
-                    // Webカメラの映像を取得するために新しいWebCamTextureを作成
-                    _webCamTexture = new WebCamTexture(width, height);
-
-                    // Webカメラの映像をスタート
-                    _webCamTexture.Play();
-
-                    // Webカメラの映像をRawImageに表示
-                    webCameraRawImage.texture = this._webCamTexture;
-
-                    // 利用可能なカメラデバイスを取得
-                    _webCamDevice = WebCamTexture.devices;
-                }
-            }
-            else
+            // Webカメラがまだセットされている場合
+            if (_webCamTexture != null)
             {
                 // Webカメラの映像からQRコードを読み取り、その結果をTextコンポーネントに表示
                 resultText.text = Read(this._webCamTexture);
+            }
+            else
+            {
+                Debug.Log("WebCamTexture is null");
             }
         }
 
@@ -80,10 +74,10 @@ namespace RicoShot.Title
         }
 
         // カメラの切り替え
-        public void ChangeCamera()
+        private void ChangeCamera()
         {
             int cameras = _webCamDevice.Length; //カメラの個数
-            if (cameras < 1) return; // カメラが1台しかなかったら実行せず終了
+            if (cameras < 1) return; // カメラが1台もなかったら実行せず終了
 
             // カメラのインデックスをインクリメント
             _selectingCamera++;
