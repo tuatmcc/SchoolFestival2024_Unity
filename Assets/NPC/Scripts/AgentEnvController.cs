d88fe317018da5db12308932a2296b55e35aa4f0
ru322 <168622173+ru322@users.noreply.github.com>
2024-10-13 23:39:01 +0900

    AgentEnvwo

--- Assets/NPC/Scripts/AgentEnvController.cs
+++ Assets/NPC/Scripts/AgentEnvController.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace NPC
 {
-    
+    // struct
+
     public class AgentEnvController : MonoBehaviour
     {
         //エージェントの能力を決定
         public float agentSpeed=0.3f;
         public float maxVelocity=50.0f;
 
-        //
+
+        
+        // public int Alpha
+        // //
+    
+        // public string AlEphaBulletTag;
+        // [SerializeField]
+        // public string BravoBulletTag;
+
+        // [SerializeField]
+        // public List<GameObject> AlphaAgents=new List<GameObject>();
+        // [SerializeField]
+        // public List<GameObject> BravoAgents=new List<GameObject>();
+
     }
 
 }
