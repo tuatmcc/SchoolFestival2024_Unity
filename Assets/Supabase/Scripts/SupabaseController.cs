81deb5850c15fa3b0d9fdaf5b9704ba1be33959b
uPiscium <upiscium@gmail.com>
2024-10-23 18:49:23 +0900

    Supabaseのクライアント無いなった...

--- Assets/Supabase/Scripts/SupabaseController.cs
+++ Assets/Supabase/Scripts/SupabaseController.cs
@@ -14,68 +14,89 @@ namespace RicoShot.Supabase
     {
         [SerializeField] private string supabaseURL;
         [SerializeField] private string supabaseKey;
-        private Client _client = null;
+        private Client _supabaseClient;
 
-        public async void Awake()
+        private async UniTask Connect()
         {
-            Debug.Log(supabaseURL);
-            Debug.Log(supabaseKey);
-            _client = new Client(supabaseURL, supabaseKey);
-            await _client.InitializeAsync();
+            await _supabaseClient.InitializeAsync();
+            Debug.Log($"Connected to {supabaseURL}");
         }
 
         public void Start()
         {
-            ;
+            var options = new SupabaseOptions()
+            {
+                AutoConnectRealtime = true
+            }; 
+            _supabaseClient = new Client(supabaseURL, supabaseKey, options);
+            Connect().Forget();
         }
 
-        public async UniTask<ProfileContainer> GetProfile(string userID)
+        public void OnClick()
         {
-            var response = await _client?.From<ProfileContainer>().Get()!;
+            string userID = "0f5d546a-dd80-5406-a005-a4f3061b9fb4";
+            string matchID = "5cbb8028-b7e1-5ec0-a482-cbaff11709b8";
+            string teamID = "16736f78-c8d8-55ff-90d2-0300ffc54b4f";
+            string playerID = "0eb05d63-02be-518c-acf4-e73c0c5cc1a3";
+            Debug.Log(_supabaseClient is null);
+            Debug.Log(userID);
+            Debug.Log(teamID);
+            Debug.Log(playerID);
+            Debug.Log(matchID);
+            var profile = GetProfile(userID).Result;
+            // var match = GetMatching(matchID).Result;
+            // var team = GetTeam(teamID).Result;
+            // var player = GetPlayer(playerID).Result;
+        }
+
+        public async Task<ProfileContainer> GetProfile(string userID)
+        {
+            Debug.Log(_supabaseClient is null);
+            var response = await _supabaseClient.From<ProfileContainer>().Get();
             return response.Models.Find(x => x.id == userID);
         }
 
         public async void UpsertProfile(ProfileContainer profile)
         {
-            await _client.From<ProfileContainer>().Upsert(profile);
+            await _supabaseClient.From<ProfileContainer>().Upsert(profile);
         }
 
-        public async UniTask<MatchingResultContainer> GetMatching(string userID)
+        public async Task<MatchingResultContainer> GetMatching(string userID)
         {
-            var response = await _client?.From<MatchingResultContainer>().Get()!;
+            var response = await _supabaseClient.From<MatchingResultContainer>().Get();
             return response.Models.Find(x => x.id == userID);
         }
 
         public async void UpsertMatching(MatchingResultContainer result)
         {
-            await _client.From<MatchingResultContainer>().Upsert(result);
+            await _supabaseClient.From<MatchingResultContainer>().Upsert(result);
         }
 
-        public async UniTask<TeamContainer> GetTeam(string teamID)
+        public async Task<TeamContainer> GetTeam(string teamID)
         {
-            var response = await _client?.From<TeamContainer>().Get()!;
+            var response = await _supabaseClient.From<TeamContainer>().Get();
             return response.Models.Find(x => x.id == teamID);
         }
 
         public async void UpsertTeam(TeamContainer team)
         {
-            await _client.From<TeamContainer>().Upsert(team);
+            await _supabaseClient.From<TeamContainer>().Upsert(team);
         }
 
-        public async UniTask<PlayerContainer> GetPlayer(string userID)
+        public async Task<PlayerContainer> GetPlayer(string userID)
         {
-            var response = await _client?.From<PlayerContainer>().Get()!;
+            var response = await _supabaseClient.From<PlayerContainer>().Get();
             return response.Models.Find(x => x.id == userID);
         }
 
         public async void UpsertPlayer(PlayerContainer player)
         {
-            await _client.From<PlayerContainer>().Upsert(player);
+            await _supabaseClient.From<PlayerContainer>().Upsert(player);
         }
 
-        public async UniTask<List<ProfilesWithStatsContainer>> GetProfilesWithStats()
+        public async Task<List<ProfilesWithStatsContainer>> GetProfilesWithStats()
         {
-            var response = await _client?.From<ProfilesWithStatsContainer>().Get()!;
+            var response = await _supabaseClient.From<ProfilesWithStatsContainer>().Get();
             return response.Models.ToList();
         }
     }
