e18d16f0aa8fe48736b08bee1679037b67fefb8f
ojii3 <84656786+OJII3@users.noreply.github.com>
2024-10-26 00:46:04 +0900

    feat: 自動でアクティブなChibiのAnimatorをとってくるように

---
+++ Assets/Chibi/Scripts/CharacterAnimatiorSettings.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Chibi
+{
+    [RequireComponent(typeof(CharacterSettingsController))]
+    public class CharacterAnimatiorSettings : MonoBehaviour
+    {
+        private Animator _animator;
+        private CharacterSettingsController _settingsController;
+
+        private void Awake()
+        {
+            _settingsController = GetComponent<CharacterSettingsController>();
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Start()
+        {
+            _animator.avatar = _settingsController.animator.avatar;
+        }
+    }
+}
