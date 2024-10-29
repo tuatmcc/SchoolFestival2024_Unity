864687a1e337d133be1f356b4574d1e41c8d1a9a
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2024-10-29 16:24:28 +0900

    Trail1

---
+++ Assets/VFX/VFXTrail1.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.VFX;
+
+namespace RicoShot.VFX
+{
+    public class VFXTrail1 : MonoBehaviour
+    {
+        [SerializeField] private VisualEffect vfx;
+        private const string RatePropertyName = "RatePerUnit";
+        [SerializeField] private Rigidbody targetRb;
+        [SerializeField] private float minimumRate = 1.0f;
+        [SerializeField] private float strength = 1.0f;
+        
+        private void FixedUpdate()
+        {
+            var rate = targetRb.velocity.magnitude;
+            rate = Mathf.Max(minimumRate, rate) - minimumRate;
+            rate *= strength;
+            Debug.Log(rate);
+            vfx.SetFloat(RatePropertyName, rate);
+        }
+    }
+}
