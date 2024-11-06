c7643e239c5c753d4db60db3c9f8b7c0fc1d1036
kitashima_desktop <110659558+kitashimauni@users.noreply.github.com>
2024-11-07 02:34:27 +0900

    再びエラーハンドリング

--- Assets/RicoShot/Scripts/Play/BulletController.cs
+++ Assets/RicoShot/Scripts/Play/BulletController.cs
@@ -57,12 +57,18 @@ namespace RicoShot.Play
                 transform.position = shooterPosition + Vector3.up * 0.5f + shooterForward * 1f;
                 rb.AddForce(shooterForward * bulletForce, ForceMode.Impulse);
                 _renderer.enabled = true;
-                EnableRendererRpc();
+                if (playSceneManager.PlayState == PlayState.Playing)
+                {
+                    EnableRendererRpc();
+                }
                 //Debug.Log("Shot");
             }
             else if (!IsServer)
             {
-                EnableInterpolateRpc();
+                if (playSceneManager.PlayState == PlayState.Playing)
+                {
+                    EnableInterpolateRpc();
+                }
             }
             if (IsServer)
             {
@@ -91,7 +97,7 @@ namespace RicoShot.Play
 
         private void OnCollisionEnter(Collision other)
         {
-            if (IsOwner & !destroying & IsSpawned)
+            if (IsOwner && !destroying && IsSpawned)
             {
                 //Debug.Log("衝突");
                 if (other.gameObject.CompareTag("Border"))
@@ -123,10 +129,12 @@ namespace RicoShot.Play
                         reflect_count = 0;
                         var hpHolder = other.gameObject.GetComponent<IHpHolder>();
                         hpHolder.DecreaseHp(damage);
-                        // nullable
-                        scoreManager.AddScoreRpc(shooterData.UUID, score);
-                        DestroyThisRpc();
-                        destroying = true;
+                        if (playSceneManager.PlayState == PlayState.Playing)
+                        {
+                            scoreManager.AddScoreRpc(shooterData.UUID, score);
+                            DestroyThisRpc();
+                            destroying = true;
+                        }
                     }
                 }
                 if (reflect_count >= max_reflect_num + 1)
@@ -134,8 +142,11 @@ namespace RicoShot.Play
                     rb.velocity = new Vector3(0, 0, 0);
                     this.transform.position = new Vector3(0, -0.4f, 0);
                     reflect_count = 0;
-                    DestroyThisRpc();
-                    destroying = true;
+                    if (playSceneManager.PlayState == PlayState.Playing)
+                    {
+                        DestroyThisRpc();
+                        destroying = true;
+                    }
                 }
             }
         }
