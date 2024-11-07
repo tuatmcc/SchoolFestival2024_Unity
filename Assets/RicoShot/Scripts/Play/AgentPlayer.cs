15b8f0101c21b13f10f3065e5b8e799873ed62f7
kitashima_desktop <110659558+kitashimauni@users.noreply.github.com>
2024-11-07 12:48:33 +0900

    脱走対策等

--- Assets/RicoShot/Scripts/Play/AgentPlayer.cs
+++ Assets/RicoShot/Scripts/Play/AgentPlayer.cs
@@ -46,8 +46,10 @@ namespace RicoShot.Play
         {
             if (rb.velocity.magnitude != 0 && playSceneManager.PlayState == PlayState.Playing)
             {
+                var holizontal = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+
                 // 移動方向に向かせる回転を計算
-                Quaternion targetRotation = Quaternion.LookRotation(rb.velocity);
+                Quaternion targetRotation = Quaternion.LookRotation(holizontal);
 
                 // 現在の回転から目標の回転へ徐々に回転
                 transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10f * Time.deltaTime);
