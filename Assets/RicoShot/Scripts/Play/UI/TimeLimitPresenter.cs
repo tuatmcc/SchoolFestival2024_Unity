5db84ab6b851cc0b38a6031f6db44ddf095092db
OJII3 <84656786+OJII3@users.noreply.github.com>
2024-11-08 03:18:03 +0900

    feat: 残り時間のUI, Networkのエラー祭り

---
+++ Assets/RicoShot/Scripts/Play/UI/TimeLimitPresenter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using R3;
+using RicoShot.Play.Interface;
+using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace RicoShot.Play.UI
+{
+    [RequireComponent(typeof(TMP_Text))]
+    public class TimeLimitPresenter : MonoBehaviour
+    {
+        [Inject] private readonly ITimeManager _timeManager;
+        private TMP_Text _timeLimitText;
+
+        private void Start()
+        {
+            _timeLimitText = GetComponent<TMP_Text>();
+            Observable.FromEvent<long>(h => _timeManager.OnPlayTimeChanged += h,
+                    h => _timeManager.OnPlayTimeChanged -= h)
+                .Subscribe(OnPlayTimeChanged).AddTo(this);
+        }
+
+        private void OnPlayTimeChanged(long playTime)
+        {
+            var time = TimeSpan.FromTicks(playTime);
+            _timeLimitText.text = $"{time.Minutes:0}:{time.Seconds:00}";
+        }
+    }
+}
