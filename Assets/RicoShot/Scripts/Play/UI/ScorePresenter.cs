953402e82d9e18ae6929cdefc587eb0369806fb7
kitashima_desktop <110659558+kitashimauni@users.noreply.github.com>
2024-11-09 02:20:59 +0900

    スコアを問題なく表示できる

--- Assets/RicoShot/Scripts/Play/UI/ScorePresenter.cs
+++ Assets/RicoShot/Scripts/Play/UI/ScorePresenter.cs
@@ -1,40 +1,53 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using R3;
+using RicoShot.Core;
 using RicoShot.Core.Interface;
 using RicoShot.Play.Interface;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 namespace RicoShot.Play.UI
 {
     public class ScorePresenter : MonoBehaviour
     {
+        [SerializeField] private Image scoreImage;
         [SerializeField] private TMP_Text scoreText;
 
-        [Inject] private readonly INetworkController _networkController;
+        [Inject] private readonly INetworkScoreManager _scoreManager;
         [Inject] private readonly ILocalPlayerManager _localPlayerManager;
         [Inject] private readonly IPlaySceneManager _playSceneManager;
+        [Inject] private readonly IGameStateManager _gameStateManager;
 
         private void Start()
         {
-            Observable.FromEvent<PlayState>(h => _playSceneManager.OnPlayStateChanged += h,
-                    h => _playSceneManager.OnPlayStateChanged -= h)
-                .Where(state => state == PlayState.Playing)
-                .Subscribe(_ => SetScoreUpdater())
-                .AddTo(this);
+            if (_gameStateManager.NetworkMode == NetworkMode.Client)
+            {
+                scoreImage.enabled = true;
+                scoreText.enabled = true;
+                scoreText.text = "0";
+                Observable.FromEvent<PlayState>(h => _playSceneManager.OnPlayStateChanged += h,
+                        h => _playSceneManager.OnPlayStateChanged -= h)
+                    .Where(state => state == PlayState.Playing)
+                    .Subscribe(_ => SetScoreUpdater())
+                    .AddTo(this);
+            }
+            else
+            {
+                scoreImage.enabled = false;
+                scoreText.enabled = false;
+            }
         }
 
         private void SetScoreUpdater()
         {
-            List<ScoreData> scores = new();
-            foreach (var score in _networkController.ScoreManager.ScoreList) scores.Add(score);
-
-            var index = scores.FindIndex(x => x.UUID == _localPlayerManager.LocalPlayerUUID);
-            Observable.FromEvent(h => _networkController.ScoreManager.ScoreList[index].OnDataChanged += h,
-                    h => _networkController.ScoreManager.ScoreList[index].OnDataChanged -= h)
-                .Subscribe(_ => scoreText.text = _networkController.ScoreManager.ScoreList[index].Score.ToString())
+            var index = _scoreManager.ScoreList.FindIndex(x => x.UUID.ToString() == _localPlayerManager.LocalPlayerUUID);
+            Debug.Log(index);
+            Observable.FromEvent(h => _scoreManager.ScoreList.OnDataChanged += h,
+                    h => _scoreManager.ScoreList.OnDataChanged -= h)
+                .Subscribe(_ => scoreText.text = _scoreManager.ScoreList[index].Score.ToString())
                 .AddTo(this);
         }
     }
