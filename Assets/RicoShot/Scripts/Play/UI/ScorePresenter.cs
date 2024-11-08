dd93fb679a9f50f1b9de92e4486e0add0741658b
OJII3 <84656786+OJII3@users.noreply.github.com>
2024-11-09 01:36:02 +0900

    feat: リアルタイムスコア表示のなれのはて(動作しないためいったん見送り)

---
+++ Assets/RicoShot/Scripts/Play/UI/ScorePresenter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using R3;
+using RicoShot.Core.Interface;
+using RicoShot.Play.Interface;
+using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace RicoShot.Play.UI
+{
+    public class ScorePresenter : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text scoreText;
+
+        [Inject] private readonly INetworkController _networkController;
+        [Inject] private readonly ILocalPlayerManager _localPlayerManager;
+        [Inject] private readonly IPlaySceneManager _playSceneManager;
+
+        private void Start()
+        {
+            Observable.FromEvent<PlayState>(h => _playSceneManager.OnPlayStateChanged += h,
+                    h => _playSceneManager.OnPlayStateChanged -= h)
+                .Where(state => state == PlayState.Playing)
+                .Subscribe(_ => SetScoreUpdater())
+                .AddTo(this);
+        }
+
+        private void SetScoreUpdater()
+        {
+            List<ScoreData> scores = new();
+            foreach (var score in _networkController.ScoreManager.ScoreList) scores.Add(score);
+
+            var index = scores.FindIndex(x => x.UUID == _localPlayerManager.LocalPlayerUUID);
+            Observable.FromEvent(h => _networkController.ScoreManager.ScoreList[index].OnDataChanged += h,
+                    h => _networkController.ScoreManager.ScoreList[index].OnDataChanged -= h)
+                .Subscribe(_ => scoreText.text = _networkController.ScoreManager.ScoreList[index].Score.ToString())
+                .AddTo(this);
+        }
+    }
+}
