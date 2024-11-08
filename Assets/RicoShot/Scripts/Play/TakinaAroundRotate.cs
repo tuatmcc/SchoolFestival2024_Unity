e9cf25fa98dbf617af72c7afc45f11c28e2ea360
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2024-11-08 10:48:42 +0900

    ステージできました 見た目はまだです

---
+++ Assets/RicoShot/Scripts/Play/TakinaAroundRotate.cs
@@ -0,0 +1,15 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace RicoShot.Play
+{
+    public class TakinaAroundRotate : MonoBehaviour
+    {
+        private void Start()
+        {
+            transform.DORotate(new Vector3(0f, 360f, 0f), 5f, RotateMode.FastBeyond360)
+                .SetLoops(-1, LoopType.Restart)
+                .SetEase(Ease.Linear);
+        }
+    }
+}
