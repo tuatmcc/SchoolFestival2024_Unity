6e550e6c6a1f4295366bda36879e22c6a10e755f
OJII3 <84656786+OJII3@users.noreply.github.com>
2024-11-09 14:29:26 +0900

    wip: ranking, not fully working

---
+++ Assets/RicoShot/Scripts/Result/UI/RankingPlayer.cs
@@ -0,0 +1,27 @@
+﻿using RicoShot.Core;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RicoShot.Result.UI
+{
+    public class RankingPlayer : MonoBehaviour
+    {
+        [SerializeField] private Image background;
+        [SerializeField] private Image rankImageArea;
+        [SerializeField] private TMP_Text displayNameArea;
+        [SerializeField] private TMP_Text scoreTextArea;
+        [SerializeField] private Sprite alphaBackground;
+        [SerializeField] private Sprite bravoBackground;
+        [SerializeField] private Sprite[] rankImages;
+
+        public void SetPlayerData(string displayName, Team team, int score, bool isLocalPlayer, int rank)
+        {
+            displayNameArea.text = displayName;
+            scoreTextArea.text = score.ToString();
+            background.sprite = team == Team.Alpha ? alphaBackground : bravoBackground;
+            for (var i = 0; i < rankImages.Length; ++i)
+                rankImageArea.sprite = rankImages[rank - 1];
+        }
+    }
+}
